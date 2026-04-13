d03e6f2b6c6e93547a483494a3593d5f190da8a9
Hamad171 <h.w.a@outlook.com>
2026-04-13 20:35:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import axios from "axios";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
